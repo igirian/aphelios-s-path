72f5ea2068e4c86e44416a6071d5f39dec06f564
hynti-admin <aureliolsolanivia@gmail.com>
2025-07-01 15:18:52 +0200

    Tutto qullo fatto prima

--- aph_2025-06-30_17-35-05/scripts/player.gd
+++ aph_2025-06-30_17-35-05/scripts/player.gd
@@ -1,22 +1,69 @@
 extends CharacterBody2D
 
-#XD
+
 
 const SPEED = 500.0
 const JUMP_VELOCITY = -1250.0
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-
 var weapon_rotation = ["severum","calibrum","gravitum","infernum","crescendum"] 
 var support_var = 0
-
+var severum_durability:int = 5
+var calibrum_durability:int = 5 
+var gravitum_durability:int = 5 
+var infernum_durability:int = 5 
+var crescendum_durability:int = 5
+#rotation after weapon is consumed
+func _rotation():
+	support_var = weapon_rotation[0]
+	var i=0
+	while i<4 :
+		weapon_rotation[i] = weapon_rotation[i+1]
+		i=i+1
+	weapon_rotation[4] = support_var
 # function to swap the weapons in the first slot of the array
+func swap_weapons():
+	
+	support_var = weapon_rotation[0]
+	weapon_rotation[0] = weapon_rotation[1]
+	weapon_rotation[1] = support_var
+	print(weapon_rotation[0])
+#all is_action_pressed have to here
 func _input(event):
 	if event.is_action_pressed("swap_weapon"):
-		support_var = weapon_rotation[0]
-		weapon_rotation[0] = weapon_rotation[1]
-		weapon_rotation[1] = support_var
+		swap_weapons()
+	if event.is_action_pressed("shoot"):
+		shoot()
+		print(weapon_rotation)
+#shoots upon pressing E
+func shoot():
+	print("shoot")
+	if weapon_rotation[0] == "calibrum":
+		calibrum_durability -= 1
+		if calibrum_durability == 0:
+			_rotation()
+			calibrum_durability = 5
+	elif weapon_rotation[0] == "severum":
+		severum_durability -= 1
+		if severum_durability == 0:
+			_rotation()
+			severum_durability = 5
+	elif weapon_rotation[0] == "gravitum":
+		gravitum_durability -= 1
+		if gravitum_durability == 0:
+			_rotation()
+			gravitum_durability = 5
+	elif weapon_rotation[0] == "infernum":
+		infernum_durability -= 1
+		if infernum_durability == 0:
+			_rotation()
+			infernum_durability = 5
+	elif weapon_rotation[0] == "crescendum":
+		crescendum_durability -= 1
+		if crescendum_durability == 0:
+			_rotation()
+			crescendum_durability = 5
 #change the sprite depending of the main weapon
 func _sprite_detect(direction):
 	if weapon_rotation[0] == "calibrum":
